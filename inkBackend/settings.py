ed4c135e0fa71156fb0c031ef76018a54c30fa9e
saihanhtet <sai.hanhtetsan@gmail.com>
2024-03-07 14:38:03 +0700

    staticfiles changed name to staticfiles_build

--- inkBackend/settings.py
+++ inkBackend/settings.py
@@ -167,7 +167,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # Specify the location for collected static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
