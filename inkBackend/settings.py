3d9158e7fa434b9faa7c3d3e69fffcda9e88cb73
saihanhtet <sai.hanhtetsan@gmail.com>
2024-03-08 16:37:20 +0700

    change debug mode

--- inkBackend/settings.py
+++ inkBackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
 from datetime import timedelta
 from pathlib import Path
@@ -18,16 +18,16 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
+print(os.getenv('DEBUG'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['inkbackend-production.up.railway.app', '127.0.0:1']
 
