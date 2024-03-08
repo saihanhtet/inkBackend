4886bb669a9196f54447c59decda5e2d15bb159a
saihanhtet <sai.hanhtetsan@gmail.com>
2024-03-08 16:51:41 +0700

    remove settings.py

--- inkBackend/settings.py
+++ inkBackend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
 from datetime import timedelta
