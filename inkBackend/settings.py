f46ade95c94aa2442706082bfdc5c4474f7cd89a
saihanhtet <sai.hanhtetsan@gmail.com>
2024-03-08 15:54:36 +0700

    add domain for allowance

--- inkBackend/settings.py
+++ inkBackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inkbackend-production.up.railway.app', '127.0.0:1']
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:5173',
