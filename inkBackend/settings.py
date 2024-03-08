d7bb3210fb7c786df204fd3d06b517c292973328
saihanhtet <sai.hanhtetsan@gmail.com>
2024-03-08 16:39:16 +0700

    change debug mode

--- inkBackend/settings.py
+++ inkBackend/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-print(os.getenv('DEBUG'))
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
