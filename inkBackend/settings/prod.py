209835060583a8873cf184a60385ad49635bd9f9
saihanhtet <sai.hanhtetsan@gmail.com>
2024-03-14 11:59:58 +0700

    added url

--- inkBackend/settings/prod.py
+++ inkBackend/settings/prod.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'https://inkbackend-production.up.railway.app/',
+    'https://lightecfa.up.railway.app/',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
